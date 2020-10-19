bb6cab853c3adb61d41c0ddfc24c18ae19c2e316
Maximilian Senten <maximilian.senten00@gmail.com>
2020-10-19 10:35:18 +0200

    Database bug fixes with launching

--- CityGoASPBackEnd/CityGoASPBackEnd/Model/DBInitializer.cs
+++ CityGoASPBackEnd/CityGoASPBackEnd/Model/DBInitializer.cs
@@ -9,9 +9,24 @@ namespace CityGoASPBackEnd.Model
     {
         public static void Initialize(Context context)
         {
+            List<User> userH = new List<User>() { 
+            new User{ 
+                Name = "Jhon Doe",
+                Username = "Jh0n D0e",
+                Balls = 5,
+                Email = "jhon.doe@gmail.com"
+            }
+            };
 
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+            if (!context.Users.Any())
+            {
+                foreach (var user in userH)
+                {
+                    context.Users.Add(user);
+                }
+            }
             context.SaveChanges();
         }
     }
