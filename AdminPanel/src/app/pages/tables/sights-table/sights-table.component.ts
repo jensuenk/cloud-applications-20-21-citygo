b907a603b3ccfaf48eddee52b18a17cb3e11926d
Jens Uenk <jens.uenk@gmail.com>
2020-10-21 22:25:33 +0200

    Created empty sights table

--- AdminPanel/src/app/pages/tables/sights-table/sights-table.component.ts
+++ AdminPanel/src/app/pages/tables/sights-table/sights-table.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+
+import { SmartTableData } from '../../../@core/data/smart-table';
 
 @Component({
   selector: 'ngx-sights-table',
   templateUrl: './sights-table.component.html',
   styleUrls: ['./sights-table.component.scss']
 })
-export class SightsTableComponent implements OnInit {
+export class SightsTableComponent {
 
-  constructor() { }
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      name: {
+        title: 'Name',
+        type: 'string',
+      },
+      info: {
+        title: 'Info',
+        type: 'string',
+      },
+      monument: {
+        title: 'Monument',
+        type: 'boolean',
+      },
+      stop: {
+        title: 'Stop',
+        type: 'string',
+      },
+      location: {
+        title: 'Location',
+        type: 'string',
+      },
+      challenge: {
+        title: 'Challenge_ID',
+        type: 'number',
+      },
+    },
+  };
 
-  ngOnInit(): void {
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: SmartTableData) {
+    const data = this.service.getData();
+    this.source.load(data);
   }
 
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
 }
