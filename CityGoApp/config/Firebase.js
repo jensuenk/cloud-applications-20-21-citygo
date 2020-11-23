aa5daefe466046b4546550246895368e5649bbe6
jb500 <55529549+jb500@users.noreply.github.com>
2020-11-23 17:16:01 +0100

    Added Firebase config

---
+++ CityGoApp/config/Firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyB55cL5zUswEM5oZj_2Y-fDfqxCsNTAa_M',
+    authDomain: 'citygo-6ca36.firebaseapp.com',
+    databaseURL: 'https://citygo-6ca36.firebaseio.com',
+    projectId: 'citygo-6ca36',
+    storageBucket: '',
+    messagingSenderId: '996768732060',
+    appId: '996768732060:web:e47e95a81e25b306fd6e59'
+}
+
+// Initialize Firebase
+const Firebase = firebase.initializeApp(firebaseConfig)
+
+export default Firebase
