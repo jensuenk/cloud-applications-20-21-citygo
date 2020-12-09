a681d2ca91eec7b79f16bf908ff00254948ceca5
Jens Uenk <jens.uenk@gmail.com>
2020-12-09 22:49:58 +0100

    Created integration test to create a new user

---
+++ CityGoASPBackEnd/Tests/CleanTesting.Application.IntegrationTests/Testing.cs
@@ -0,0 +1,123 @@
+﻿using CityGoASPBackEnd;
+using Infrastucture.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using Respawn;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CleanTesting.Application.IntegrationTests
+{
+    [SetUpFixture]
+    public class Testing
+    {
+        private static IConfiguration _configuration;
+        private static IServiceScopeFactory _scopeFactory;
+        private static Checkpoint _checkpoint;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+                w.EnvironmentName == "Development" &&
+                w.ApplicationName == "CleanTesting.WebUI"));
+
+            var startup = new Startup(_configuration);
+
+            startup.ConfigureServices(services);
+
+            _scopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
+
+            _checkpoint = new Checkpoint
+            {
+                TablesToIgnore = new[] { "__EFMigrationsHistory" }
+            };
+
+            EnsureDatabase();
+        }
+
+        public void EnsureDatabase()
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<DBContext>();
+
+            context.Database.Migrate();
+        }
+
+        public static async Task ResetState()
+        {
+            await _checkpoint.Reset(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public static async Task<TEntity> FindAsync<TEntity>(int id)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<DBContext>();
+
+            return await context.FindAsync<TEntity>(id);
+        }
+
+        public static async Task AddAsync<TEntity>(TEntity entity)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<DBContext>();
+
+            context.Add(entity);
+
+            await context.SaveChangesAsync();
+        }
+
+        /*
+        public static async Task<string> RunAsDefaultUserAsync()
+        {
+            return await RunAsUserAsync("test@local", "Testing1234!");
+        }
+
+        public static async Task<string> RunAsUserAsync(string userName, string password)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var user = new ApplicationUser { UserName = userName, Email = userName };
+
+            var result = await userManager.CreateAsync(user, password);
+
+            _currentUserId = user.Id;
+
+            return _currentUserId;
+        }
+        */
+
+        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var mediator = scope.ServiceProvider.GetService<IMediator>();
+
+            return await mediator.Send(request);
+        }
+    }
+}
