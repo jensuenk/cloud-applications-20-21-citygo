3a03bb74800f2f840564995406794a106b86d574
Stef Martens <stef.m@hotmail.be>
2020-12-11 19:57:45 +0100

    Checkboxes en link met Users  zijn data gekoppeld! :)

--- CityGoApp/screens/InventoryScreen.js
+++ CityGoApp/screens/InventoryScreen.js
@@ -7,6 +7,7 @@ import {
   Image
 } from 'react-native';
 import { CheckBox } from 'react-native-elements'
+import { TabRouter } from 'react-navigation';
 
 
 export default class InventoryScreen extends React.Component {
@@ -14,20 +15,97 @@ export default class InventoryScreen extends React.Component {
     super();
     this.state = {
       sights: [],
-      voltooideChallenges:[]
+      voltooideChallenges: [],
+      booleans: [],
+      voorlopigebool: false,
+      i: 0,
+      intOmTeChecken: 0,
     }
   }
 
+
+
   componentDidMount() {
+    this.ivoorcheckboxes = 0
     this.apiCall();
 
   }
 
+  //voorlopig enkel user 1 aangezien login nog niet helemaal werkt
   async apiCall() {
     let resp = await fetch('https://citygo5.azurewebsites.net/sights')
     let respJson = await resp.json();
     this.setState({ sights: respJson.sights })
+    let resp2 = await fetch('https://citygo5.azurewebsites.net/users/1')
+    let respJson2 = await resp2.json();
+    this.setState({ voltooideChallenges: respJson2.challenges })
+
+    this.state.sights.forEach(sight => {
+
+
+      this.checkenVanSightChallenge(sight)
+      if (this.state.intOmTeChecken > 0) {
+        const lijst = this.state.booleans;
+        lijst.push(true);
+        this.setState({ booleans: lijst })
+      }
+      else {
+        const lijst = this.state.booleans;
+        lijst.push(false);
+        this.setState({ booleans: lijst })
+      }
+
+      this.setState({ intOmTeChecken: 0 });
+    });
+
+
+
   }
+
+  checkenVanSightChallenge(sight) {
+    this.state.voltooideChallenges.forEach(voltooidech => {
+
+      if (sight.challenges[0].challengeId == voltooidech.challengeId) {
+        let i = this.state.intOmTeChecken;
+        i++;
+        this.setState({ intOmTeChecken: i })
+      }
+
+    });
+  }
+
+  // Om middelpunt van polygon te krijgen
+  coordinate(coordinates) {
+    let x = coordinates.map(c => c.latitude)
+    let y = coordinates.map(c => c.longitude)
+
+    let minX = Math.min.apply(null, x)
+    let maxX = Math.max.apply(null, x)
+
+    let minY = Math.min.apply(null, y)
+    let maxY = Math.max.apply(null, y)
+
+    return {
+      latitude: (minX + maxX) / 2,
+      longitude: (minY + maxY) / 2
+    }
+  }
+
+  renderItem(item, index) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.itemText}>  {item.name}</Text>
+        <View style={styles.checkbox}>
+        <CheckBox
+          checkedIcon={<Image source={require('../assets/checked.png')} />}
+          uncheckedIcon={<Image source={require('../assets/unchecked.png')} />}
+          checked={this.state.booleans[index]}
+        />
+        </View>
+      </View>
+    )
+  }
+
 
   // er moet nog weergegeven of het voltooid is of niet met bv vinkje
   render() {
@@ -35,17 +113,9 @@ export default class InventoryScreen extends React.Component {
       <View style={styles.container}>
         <Text style={styles.header}>COMPLETED SIGHTS</Text>
         <FlatList
+          renderItem
           data={this.state.sights}
-          renderItem={({ item }) =>
-            <View>
-              <Text style={styles.item}>  {item.name}</Text>        
-              <CheckBox
-              checkedIcon={<Image source={require('../assets/checked.png')} />}
-              uncheckedIcon={<Image source={require('../assets/unchecked.png')} />}
-              checked="false"
-            />
-            </View>
-          }
+          renderItem={({ item, index }) => this.renderItem(item, index)}
           keyExtractor={(item, index) => index.toString()}
         />
       </View>
@@ -74,6 +144,17 @@ const styles = StyleSheet.create({
     fontSize: 30,
     borderColor: 'white',
     borderWidth: 2,
-    color: 'white'
+    color: 'white',
+    height:100
+  },
+  itemText: {
+    fontSize: 40,
+    color: 'white',
+    width: "70%"
+  },
+  checkbox: {
+    position: 'absolute',
+    right: 0,
+    width: "30%",
   }
 })
--- CityGoApp/screens/MapZelf.js
+++ CityGoApp/screens/MapZelf.js
@@ -81,11 +81,6 @@ export default class Mapke extends React.Component {
               }
             }
 
-            // even hardcoded erin voor testing
-            this.AlertChallenge();
-
-
-
           })
 
         }
