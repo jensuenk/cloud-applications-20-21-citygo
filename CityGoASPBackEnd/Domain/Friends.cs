0957e881251d9f8bf8247ed33b707c8839341df5
Max <maximilian.senten00@gmail.com>
2020-12-16 14:02:40 +0100

    aanpassing friend klasse

--- CityGoASPBackEnd/Domain/Friends.cs
+++ CityGoASPBackEnd/Domain/Friends.cs
@@ -8,18 +8,16 @@ namespace Domain
 {
     public class Friends
     {
+        [Key]
         public int ID { get; set; }
+
         public int UserId { get; set; }
-
         public bool AcceptedUser1 { get; set; }
-
         public User User { get; set; }
 
-        public User Friend { get; set; }
 
         public int FriendId { get; set; }
-
         public bool AcceptedUser2 { get; set; }
-
+        public User Friend { get; set; }
     }
 }
