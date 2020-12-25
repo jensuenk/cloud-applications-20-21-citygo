a1bfd1624d81439a98096c4f3a4611003f5eb5ff
jb500 <55529549+jb500@users.noreply.github.com>
2020-12-25 12:12:55 +0100

    API call validation works (username check)

--- CityGoApp/screens/RegisterScreen.js
+++ CityGoApp/screens/RegisterScreen.js
@@ -16,9 +16,28 @@ class RegisterScreen extends React.Component {
             username: '',
             isValidEmail: true,
             isValidPassword: true,
-            isInvalidInput:true,
-            isvalidUsername:true,
-        };
+            isInvalidInput: true,
+            isvalidUsername: true,
+            users: [],
+            userId: 1,
+            name: "",
+            username: "",
+            email: "",
+            balls: 0,
+            friendIds: null,
+            items: null,
+            usersItems: [],
+            challenges: [],
+            friends: null,
+            userFriends: null,
+        }
+    }
+
+    async apiCallUsers() {
+        let responseApiUsers = await fetch('https://citygoaspbackend20201224141859.azurewebsites.net/Users/');
+        let responseJsonUsers = await responseApiUsers.json();
+        this.setState({ users: responseJsonUsers.users })
+        console.log("API werd gecalld");
     }
 
     handleSignUp = () => {
@@ -52,92 +71,118 @@ class RegisterScreen extends React.Component {
         }
     }
 
-    handleValidUsername = (val) => {
+    /*
+    handleLengthUsername = (val) => {
         if (val.trim().length >= 1) {
-            this.state.isvalidUsername = true;
+            //this.state.isvalidUsername = true;
         }
         else {
             this.state.isvalidUsername = false;
         }
+    }*/
+
+    handleValidUsername = (val) => {
+        //console.log("username getypt:", val);
+
+        for (let item of this.state.users) {
+            //console.log("username: ", item.username);
+            if (item.username == val) {
+                //console.log("username matcht");
+                //this.state.isvalidUsername = false;
+                //console.log("staat: ", this.state.isvalidUsername);
+                alert("De ingevoerde username " + item.username + " is al in gebruik. Probeer een andere username.");
+                this.state.isvalidUsername = false;
+                break;
+            }            
+            else {
+                //console.log("username matcht niet");
+                this.state.isvalidUsername = true;
+            }
+        }
+    }
+
+    componentDidMount = () => {
+        this.apiCallUsers();
     }
 
     handleRegister = () => {
-            if(this.state.isvalidUsername){
-                console.log("username: ", this.state.username);
-                if(this.state.isValidPassword){
-                    console.log("password: ", this.state.password);
-                    if(this.state.isValidEmail){
-                        console.log("email: ", this.state.email);
-                        this.handleSignUp();
-                    }else{
-                        alert("There is still invalid input, check each field before pushing the registration button.");
-                    }
-                }else{
+        if (this.state.isvalidUsername) {
+            console.log("username: ", this.state.username);
+            if (this.state.isValidPassword) {
+                console.log("password: ", this.state.password);
+                if (this.state.isValidEmail) {
+                    console.log("email: ", this.state.email);
+                    this.handleSignUp();
+                } else {
                     alert("There is still invalid input, check each field before pushing the registration button.");
                 }
-            }else{
+            } else {
                 alert("There is still invalid input, check each field before pushing the registration button.");
             }
+        } else {
+            alert("There is still invalid input, check each field before pushing the registration button.");
+        }
     }
 
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>Create your profile here!</Text>
-                <InputField
-                    labelValue={this.state.username}
-                    onChangeText={(username) => this.setState({ username }, (username) => this.handleValidUsername(this.state.username))}
-                    placeholderText="Username"
-                    iconType="user"
-                    secureTextEntry={false}
-                    autoCapitalize="none"
-                />
-                {this.state.isvalidUsername ? null :
-                    <Text style={styles.errorMessage}>The username has to be longer than 1 char.</Text>
-                }
+    /*{
+    this.state.isvalidUsername ? null :
+        <Text style={styles.errorMessage}>The username has already been taken. Try another username</Text>
+    }*/
 
 
-                <InputField
-                    labelValue={this.state.email}
-                    onChangeText={(email) => this.setState({ email }, (email) => this.handleValidEmail(this.state.email))}
-                    placeholderText="Email"
-                    iconType="user"
-                    secureTextEntry={false}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
-                {this.state.isValidEmail ? null :
-                    <Text style={styles.errorMessage}>The email needs to look like xxx@domain</Text>
-                }
+render() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Create your profile here!</Text>
+            <InputField
+                labelValue={this.state.username}
+                onChangeText={(username) => this.setState({ username }, (username) => this.handleValidUsername(this.state.username))}
+                placeholderText="Username"
+                iconType="user"
+                secureTextEntry={false}
+                autoCapitalize="none"
+            />
 
-                <InputField
-                    labelValue={this.state.password}
-                    onChangeText={(password) => this.setState({ password }, (password) => this.handleValidPassword(this.state.password))}
-                    placeholderText="Password"
-                    iconType="lock"
-                    secureTextEntry={true}
-                />
-                {this.state.isValidPassword ? null :
-                    <Text style={styles.errorMessage}>The password needs to be 6 characters long</Text>
-                }
+            <InputField
+                labelValue={this.state.email}
+                onChangeText={(email) => this.setState({ email }, (email) => this.handleValidEmail(this.state.email))}
+                placeholderText="Email"
+                iconType="user"
+                secureTextEntry={false}
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            {this.state.isValidEmail ? null :
+                <Text style={styles.errorMessage}>The email needs to look like xxx@domain</Text>
+            }
 
-                <View>
-                    <Text style={styles.textPrivate}>By registering you agree with our Terms of Service and Privacy Policy.</Text>
-                </View>
+            <InputField
+                labelValue={this.state.password}
+                onChangeText={(password) => this.setState({ password }, (password) => this.handleValidPassword(this.state.password))}
+                placeholderText="Password"
+                iconType="lock"
+                secureTextEntry={true}
+            />
+            {this.state.isValidPassword ? null :
+                <Text style={styles.errorMessage}>The password needs to be 6 characters long</Text>
+            }
 
-                <StandardButton
-                    buttonTitle="Register Now"
-                    onPress={() => { this.handleRegister() }}
-                />                
-
-                <TouchableOpacity style={styles.forgotButton} onPress={() => this.props.navigation.navigate('LoginScreen')}>
-                    <Text style={styles.navButtonText} >Already registered? Sign in</Text>
-                </TouchableOpacity>
-
+            <View>
+                <Text style={styles.textPrivate}>By registering you agree with our Terms of Service and Privacy Policy.</Text>
             </View>
-        );
-    }
+
+            <StandardButton
+                buttonTitle="Register Now"
+                onPress={() => { this.handleRegister() }}
+            />
+
+            <TouchableOpacity style={styles.forgotButton} onPress={() => this.props.navigation.navigate('LoginScreen')}>
+                <Text style={styles.navButtonText} >Already registered? Sign in</Text>
+            </TouchableOpacity>
+
+        </View>
+    );
+}
 }
 
 export default RegisterScreen;
