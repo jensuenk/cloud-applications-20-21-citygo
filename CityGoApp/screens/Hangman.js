0ec14f032aaeeb4fab3d50a989e6a7086f444e00
Philip De Rudder <s102279@ap.be>
2020-12-25 16:17:29 +0100

    add hangman screen

---
+++ CityGoApp/screens/Hangman.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Button, TouchableHighlight, Alert  } from 'react-native';
+
+export default class Hangman extends React.Component {
+
+}
