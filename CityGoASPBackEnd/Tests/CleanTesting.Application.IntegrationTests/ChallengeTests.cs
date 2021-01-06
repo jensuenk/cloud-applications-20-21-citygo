7e7555c25fbeffca6bb3ae88dce1f7450aec6663
Max <maximilian.senten00@gmail.com>
2021-01-06 18:03:30 +0100

    Sight tests

--- CityGoASPBackEnd/Tests/CleanTesting.Application.IntegrationTests/ChallengeTests.cs
+++ CityGoASPBackEnd/Tests/CleanTesting.Application.IntegrationTests/ChallengeTests.cs
@@ -38,7 +38,7 @@ namespace CleanTesting.Application.IntegrationTests
         }
 
         [Test]
-        public async Task ShouldAddItemToChallenge()
+        public async Task ShouldAddChallengeToUser()
         {
             var newChallenge = new ChallengeVM()
             {
@@ -69,10 +69,57 @@ namespace CleanTesting.Application.IntegrationTests
             var command3 = new AddChallengeToUserCommand(user.UserId, challenge.ChallengeId);
             var cancelationToken3 = await SendAsync(command3);
 
-            var resultUserChallenges = await FindAsyncComposite<UsersChallenges>(1, 1);
+            var resultUserChallenges = await FindAsyncComposite<UsersChallenges>(user.UserId, challenge.ChallengeId);
 
             resultUserChallenges.ChallengeId.Should().Be(challenge.ChallengeId);
             resultUserChallenges.UserId.Should().Be(user.UserId);
+        }
+
+        [Test]
+        public async Task ShouldDeleteChallengeWithRelation()
+        {
+            var newChallenge = new ChallengeVM()
+            {
+                Name = "TestChal",
+                QuestionChallenge = "Vraag",
+                Answer = "Antwoord",
+                Score = 10,
+                Task = "Taak"
+            };
+
+            var command1 = new CreateChallengeCommand(newChallenge);
+            var cancelationToken1 = await SendAsync(command1);
+            var challenge = await FindAsync<Challenge>(1);
+
+            challenge.UsersChallenges.Should().BeNull();
+
+            var newUser = new UserVM()
+            {
+                Name = "Test",
+                Username = "Test123",
+                Email = "test@gmail.com",
+                Balls = 10
+            };
+            var createCommand2 = new CreateUserCommand(newUser);
+            var cancelationToken2 = await SendAsync(createCommand2);
+            var user = await FindAsync<User>(1);
+
+            var command3 = new AddChallengeToUserCommand(user.UserId, challenge.ChallengeId);
+            var cancelationToken3 = await SendAsync(command3);
+
+            var resultUserChallenges = await FindAsyncComposite<UsersChallenges>(user.UserId, challenge.ChallengeId);
+
+            resultUserChallenges.ChallengeId.Should().Be(challenge.ChallengeId);
+            resultUserChallenges.UserId.Should().Be(user.UserId);
+
+            var command4 = new DeleteChallengeCommand(challenge.ChallengeId);
+            var cancelationToken4 = await SendAsync(command4);
+            var challenge2 = await FindAsync<Challenge>(1);
+
+            challenge2.Should().BeNull();
+
+            var resultUserChallenges2 = await FindAsyncComposite<UsersChallenges>(user.UserId, challenge.ChallengeId);
+            resultUserChallenges2.Should().BeNull();
         }
     }
 }
